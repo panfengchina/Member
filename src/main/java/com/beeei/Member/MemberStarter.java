674459d1b8759757e82a187615b5c514d0c59d08
feng.pan <feng.pan@mobilemd.cn>
2019-12-13 16:15:14 +0800

    修改

---
+++ src/main/java/com/beeei/Member/MemberStarter.java
@@ -0,0 +1,12 @@
+package com.beeei.Member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberStarter {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(MemberStarter.class, args);
+    }
+}
